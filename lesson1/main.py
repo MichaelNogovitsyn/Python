df0dc479ccb52c106d48bed81cfeaa8d59b0ddc0
mexatron <mexatron>
2023-01-27 19:56:30 +0200

    init

---
+++ lesson1/main.py
@@ -0,0 +1,31 @@
+print('Hello')
+a=10
+b=1.23
+s='name'
+print(type(b))
+str = 'Hello world'
+print('{} - {} - {}'.format(a,b,s))
+print(f'{a} - {s} - {b}')
+
+F=True
+print(F)
+col = ['hello',1,True]
+
+list=[1,2,3]
+print(list)
+print(col)
+
+print('input a')
+a=int(input())
+print('input b')
+b=int(input())
+c=a//b
+c=a*b
+c=a**b
+c=round(a*b,5)
+
+if a>b:
+    print('a > b')
+else:
+    print('a < b')
+
