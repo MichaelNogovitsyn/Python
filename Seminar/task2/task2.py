e4541ee4f26dfbc8cd9187e47bd500d7bbb620c0
mexatron <mexatron>
2023-01-27 20:13:19 +0200

    task2 done

--- Seminar/task2/task2.py
+++ Seminar/task2/task2.py
@@ -1,1 +1,12 @@
 # Найдите сумму цифр трехзначного числа.
+
+n=int(input('Введите 3х-значное число: '))
+
+def SumDigit(n):
+    num3=n%10
+    num2=n//10%10
+    num1=n//100
+    sum=num1+num2+num3
+    return sum
+
+print("Сумма чисел = ",SumDigit(n))
