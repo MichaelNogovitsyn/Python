1deffd47793071c52b12139b5cdf72e0d163f7f0
mexatron <mexatron>
2023-02-05 09:00:44 +0200

    add tasks

--- python_learninig.py
+++ python_learninig.py
@@ -14,3 +14,14 @@ arr =list(map(int,input('Enter data:').split(' ')))
 print(arr)
 
 print(nums[-k-1:]+nums[:k])
+
+#####
+d=[{"V":"S001"},{"V":"S002"},{"VI":"S001"},{"VI":"S005"},{"VII":"S005"},
+{"V":"S009"},{"VIII":"S007"}]
+
+s=set()
+for element in d:
+    for value in element.values():
+        s.add(value)
+print(s)
+##########
