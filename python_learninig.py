59cfbec400648f3d6da7803f50b2578cbde85ae4
mexatron <mexatron>
2023-02-05 15:23:06 +0200

    task16 is done

--- python_learninig.py
+++ python_learninig.py
@@ -1,3 +1,5 @@
+from random import randint
+
 nums=[1,2,3,4,5,6,7,8,9,10]
 unic=[1,2,3] 
 for element in nums:
@@ -25,3 +27,5 @@ for element in d:
         s.add(value)
 print(s)
 ##########
+
+a=[0]*n
